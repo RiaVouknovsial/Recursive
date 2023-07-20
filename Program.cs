9bf9a9adc985ce43a5c5990a07145f106c8fc59f
RiaVouKnovsial <volovik.raisa.kam@gmail.com>
2023-07-20 20:58:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать программу рекусривного обхода каталогов на диске
+
+using System;
+using System.IO;
+
+
+    string path = @"C:\";
+    RecursiveDirectoryTraversal(path);
+    Console.WriteLine("Обход каталогов диска завершен");
+    Console.ReadLine();
+    
+
+    static void RecursiveDirectoryTraversal(string path)
+    {
+        try
+        {
+           string[] items = Directory.GetFileSystemEntries(path);
+
+            foreach (string item in items)
+            {
+                if (Directory.Exists(item))  
+                {
+                  RecursiveDirectoryTraversal(item);
+                }
+                else
+                {
+                  Console.WriteLine(item);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Произошла ошибка: " + e.Message);
+        }
+    }
+
+
+
+
+
